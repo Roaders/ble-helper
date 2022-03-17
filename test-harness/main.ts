4b1fb9548808774dee859e33eea27fdedb0dc01d
Giles Roadnight <giles.roadnight@gmail.com>
2022-03-17 17:59:31 +0000

    Setup test-harness prod build

--- test-harness/main.ts
+++ test-harness/main.ts
@@ -1,10 +1,14 @@
 import 'reflect-metadata';
 import 'zone.js';
-
+import { environment } from './environments/environment';
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { getRegisteredTypesWithFactories } from '@morgan-stanley/needle';
+
+if (environment.production) {
+    enableProdMode();
+}
 
 platformBrowserDynamic(getRegisteredTypesWithFactories())
     .bootstrapModule(AppModule)
